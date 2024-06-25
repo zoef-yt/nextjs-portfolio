f1d76309c2f6225ea7f596815801417cac50452e
Zoef Shaikh <67889207+zoef-yt@users.noreply.github.com>
2024-06-25 11:02:45 +0530

    Update page.tsx

--- nextjs-portfolio/app/page.tsx
+++ nextjs-portfolio/app/page.tsx
@@ -1,6 +1,6 @@
-import { About } from '@/components/aboutSection';
-import { Header } from '@/components/Header';
-import { Section } from '@/components/Section';
+// import { About } from '@/components/aboutSection';
+// import { Header } from '@/components/Header';
+// import { Section } from '@/components/Section';
 
 export default function Home() {
 	// const jsonLd = {
@@ -11,19 +11,22 @@ export default function Home() {
 	// 	description: 'product.description',
 	// };
 	return (
-		<div className='mx-auto min-h-screen max-w-screen-xl px-6 font-sans md:px-12 lg:px-24 lg:py-0 bg-background'>
-{/* 			<script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} /> */}
-			<div className='lg:flex lg:justify-between lg:gap-4'>
-				<Header />
-				<main className='flex-1'>
-					<About />
-					<Section id='Experience'>
-						Experience
-						<a href='#Projects'> To PROJECTS</a>
-					</Section>
-					<Section id='Projects'>Projects</Section>
-				</main>
-			</div>
+		<div className='min-h-screen bg-background flex flex-col items-center justify-center'>
+			<h1 class='text-white'>Work in progress</h1>
 		</div>
+		// <div className='mx-auto min-h-screen max-w-screen-xl px-6 font-sans md:px-12 lg:px-24 lg:py-0 bg-background'>
+		// <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} /> 
+		// 	<div className='lg:flex lg:justify-between lg:gap-4'>
+		// 		<Header />
+		// 		<main className='flex-1'>
+		// 			<About />
+		// 			<Section id='Experience'>
+		// 				Experience
+		// 				<a href='#Projects'> To PROJECTS</a>
+		// 			</Section>
+		// 			<Section id='Projects'>Projects</Section>
+		// 		</main>
+		// 	</div>
+		// </div>
 	);
 }
